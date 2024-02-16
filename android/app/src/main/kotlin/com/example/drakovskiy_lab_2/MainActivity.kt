de62277e4571a793a57747f1065781a0cb1e7844
DevLevKek <gamedev.lev@bk.ru>
2024-02-16 12:03:01 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/drakovskiy_lab_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.drakovskiy_lab_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Row(
+          children: [
+            Expanded(
+                child:Column(
+                    children: [
+                      Expanded(
+                          child: Container(color: Colors.black, margin: EdgeInsets.all(4),),
+                          flex:3
+                      ),
+                      Expanded(
+                          child: Container(color: Colors.teal, margin: EdgeInsets.all(4),),
+                          flex:1
+                      ),
+                      Expanded(
+                          child: Container(color: Colors.red, margin: EdgeInsets.all(4),),
+                          flex:1
+                      ),
+                      Expanded(
+                        child: Container(color: Colors.indigoAccent, margin: EdgeInsets.all(4),),
+                        flex: 3,
+                      )
+                    ]
+                )
+            ),
+            Expanded(
+                child:Column(
+                    children: [
+                      Expanded(
+                          child: Container(color: Colors.tealAccent, margin: EdgeInsets.all(4),),
+                          flex:3
+                      ),
+                      Expanded(
+                          child: Container(color: Colors.black, margin: EdgeInsets.all(4),),
+                          flex:1
+                      ),
+                      Expanded(
+                          child: Container(color: Colors.orange, margin: EdgeInsets.all(4),),
+                          flex:1
+                      ),
+                      Expanded(
+                        child: Container(color: Colors.yellowAccent, margin: EdgeInsets.all(4),),
+                        flex: 3,
+                      )
+                    ]
+                )
+            ),
+          ],
+        ),
+
+      ),
+    );
+  }
+}
